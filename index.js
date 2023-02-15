5514f8fa49232c675943e449aff78f05e95b13a7
singhkaran1203 <ks29091@gmail.com>
2023-02-16 00:08:40 +0530

    cloudinary added

--- index.js
+++ index.js
@@ -2,31 +2,38 @@ const express=require("express");
 const app=express();
 const env=require("dotenv")
 const multer=require("multer")
+const cloudinary=require("./utils/cloudinary")
 const cors=require("cors");
-const path=require("path")
+const path=require("path");
+const { urlencoded } = require("express");
 
 
 env.config()
 app.use(express.json())
 app.use(cors())
 app.use("/images",express.static(path.join(__dirname,"/images")));
+app.use(urlencoded({limit:"50mb",extended:false}))
 require("./db")
 
-const storage=multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null,"images");
-    },
-    filename: (req,file,cb)=>{
-        cb(null,req.body.name)
-    },
-});
-   
+const storage=multer.diskStorage({})
+
 const upload=multer({storage:storage});
 
+// app.post("/api/upload",upload.single("file"),(req,res)=>{
+//     res.status(200).json("file uploaded successfully")
+// }) 
+app.post('/api/upload', upload.single("img"),async (req, res) => {
+    try{
+    // console.log("file details: ", req.file);
+    // Upload the image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+    // console.log("result: ", result);
+    res.status(200).send(result.url);
+}
+catch(err){console.log("this   ",err)}
+  });  
+  
 
-app.post("/api/upload",upload.single("file"),(req,res)=>{
-    res.status(200).json("file uploaded successfully")
-})
 
 app.use("/api/auth",require("./routes/auth"))
 app.use("/api/user",require("./routes/user"))
